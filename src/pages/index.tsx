67f21b866cda691b496aa833997873cfe8dbcd4c
murilokrugner <murilokrugner@hotmail.com>
2021-02-27 00:02:59 -0300

    aula 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,69 @@
+import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+
+import { ChallengesProvider } from '../context/ChallengesContext';
 import CompleteChallenges from '../components/CompleteChallenges';
 import CountDown from '../components/CountDown';
 import ExperienceBar  from '../components/ExperienceBar';
 import Profile  from '../components/Profile';
 import ChallengeBox from '../components/ChallengeBox';
 
-import Head from 'next/head';
-
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../context/CountdownContext';
 
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
-    <div className={styles.container}>   
-      <Head>
-        <title>Início | move.it</title>
-      </Head>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>   
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
 
-      <ExperienceBar />
+        <ExperienceBar />
 
-      <CountdownProvider>     
-        <section>
-          <div>
-            <Profile />
-            <CompleteChallenges />
-            <CountDown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider>     
+          <section>
+            <div>
+              <Profile />
+              <CompleteChallenges />
+              <CountDown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps : GetServerSideProps = async (context) => {
+  // chamada api
+  const user = {
+    level: 1,
+    currentExperience: 50,
+    challengesCompleted: 2,
+  }
+
+  //cookies;
+  const { level, currentExperience, challengesCompleted } = context.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    },
+  }
 }
